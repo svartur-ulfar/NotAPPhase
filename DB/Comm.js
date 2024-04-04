ef5486b1e325342fe4ce76ee24d9696528d1fcba
Nadia <stefaniadinoiu@yahoo.com>
2024-04-04 17:39:11 +0300

    Delete DB directory

--- DB/Comm.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const commSchema = mongoose.Schema({
-    comm: String,
-    profile_name: String
-});
-
-module.exports = mongoose.model('Comm', commSchema);
--- DB/Photo.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const photoSchema = mongoose.Schema({
-    filename: String,
-    date: String,
-    s3_file: String
-});
-
-module.exports = mongoose.model('Photo', photoSchema);
